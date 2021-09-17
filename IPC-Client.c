b6f4b82f9181be5c167c9c98d74f2afa68123e3d
Charles Zhang <charleszhang418@hotmail.com>
2021-09-17 04:11:27 -0400

    Maybe IPC client

---
+++ IPC-Client.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+#define PORT 0
+#define CLIENTPORT 0
+
+void main() {
+    int ClientSocket, bindfd, connfd;
+    struct sockaddr_in clientAddr;
+    struct sockaddr_in serverAddr;
+    char buf[1024];
+
+    if ((ClientSocket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("Socket build failure");
+        exit(1);
+    }
+
+    memset(&ClientSocket, 0, sizeof(clientAddr));
+
+    clientAddr.sin_family = AF_UNIX;
+    clientAddr.sin_port = CLIENTPORT;
+    clientAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if ((bindfd = bind(ClientSocket, (struct sockaddr*)&clientAddr, sizeof(clientAddr))) == -1) {
+        perror("Bind failure");
+        exit(1);
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_UNIX;
+    serverAddr.sin_port = htnos(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if ((connfd = connect(ClientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr))) == -1) {
+        perror("Connection error");
+        exit(1);
+    }
+
+    while(1) {
+        recv(ClientSocket, buf, 1024, 0);
+        if (!strcmp(buf, "disconnect\n")) {
+            break;
+        }
+        memset(buf, 0, sizeof(buf));
+    }
+    close(ClientSocket);
+    exit(0);
+}
